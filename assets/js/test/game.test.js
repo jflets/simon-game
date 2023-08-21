154608c26b7f27da3eeb5db872e9f4695ea4323c
jflets <jfletorides02@gmail.com>
2023-08-21 11:51:13 +0100

    Install jest

--- assets/js/test/game.test.js
+++ assets/js/test/game.test.js
@@ -1,3 +1,11 @@
 /**
  * @jest-environment jsdom
  */
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContent = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContent);
+    document.close();
+})
